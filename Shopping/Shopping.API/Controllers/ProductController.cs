e97ff11524e44188d4a4455fb687fbc321f34277
inamul-haq-confiz <inamul.haq@confiz.com>
2022-10-29 18:27:43 +0500

    initial

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext context;
+        private readonly ILogger<ProductController> logger;
+
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await context
+                            .Products
+                            .Find(p => true)
+                            .ToListAsync();
+        }
+
+    }
+}
